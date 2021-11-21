691378ec5054e1c05c71995f96bd7abd9287e123
julioandre275 <julioandre275@gmail.com>
2021-11-21 09:59:55 +0100

    Added Gadd garage function

--- src/components/GaragesBody/GaragesBody.tsx
+++ src/components/GaragesBody/GaragesBody.tsx
@@ -1,6 +1,6 @@
 
-import { Box,  Container, createTheme, CssBaseline, Grid,  IconButton,  Paper } from '@mui/material';
-
+import { Box,  Container, createTheme, CssBaseline, Grid,  IconButton,  Paper , Fab, Dialog, DialogTitle, DialogActions, Button, TextField, MenuItem} from '@mui/material';
+import AddIcon from '@mui/icons-material/Add';
 
 
 import React from 'react'
@@ -37,6 +37,14 @@ const theme = createTheme({
   })
 
 const GaragesBody:React.FC=()=>{
+    const [open, setOpen] = React.useState(false);
+    const handleClose = () => {
+      setOpen(false);
+    };
+    const handleClickOpen = () => {
+        setOpen(true);
+    };
+
     const classes = useStyles();
     const searchtext = "Looking for you car"
     return(
@@ -80,6 +88,61 @@ const GaragesBody:React.FC=()=>{
                     </Box>
                 </Grid>
             </Grid>
+            <Fab color="primary" onClick={handleClickOpen} aria-label="add" sx={{ position:"fixed", margin:"76% 0% 0% 80%"}}>
+                <AddIcon />
+            </Fab>
+            
+            <Dialog
+                PaperProps={{
+                    style: {
+                        backgroundColor: "white",
+                        boxShadow: "none"
+                    },
+                    }}
+                    open={open}
+                    onClose={handleClose}
+                    aria-labelledby="alert-dialog-title"
+                    aria-describedby="alert-dialog-description"
+                    
+                >
+                    <DialogTitle id="alert-dialog-title">
+                    {"Add a new Car"}
+                    </DialogTitle>
+                <DialogActions>
+                <Container>
+                <TextField
+                    
+                    id="outlined-disabled"
+                    label="Car Number"
+                    
+                    
+                    
+                    />
+                
+                    <TextField
+                    
+                    id="outlined-disabled" 
+                    label="Mileage"
+                    
+                    sx={{ my:2 }}
+                    
+                    
+                    ><MenuItem> KM </MenuItem> </TextField>
+                    <TextField
+                    disabled
+                    id="outlined-disabled"
+                    label="Last Visit"
+                    defaultValue="12/10/2021"
+                    />
+                <Container sx={{ margin:"3% 0% 0% 0%", paddingLeft:"0%"}}>
+                <Button color="secondary" onClick={handleClose}>Save</Button>
+                <Button color="error" onClick={handleClose}>Close</Button>
+                </Container>
+                </Container>
+               
+                </DialogActions>
+                        </Dialog>
+                        
             {arrayIndexingWithLength(3).map((v)=>(
 
                      <CardComponent/>
